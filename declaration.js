b7f736774a03743be1213f212630fcff5b7b838e
Kirill Medvedev <repkomail@gmail.com>
2024-02-15 22:06:51 +0200

    feat: add constants

---
+++ declaration.js
@@ -0,0 +1,6 @@
+export const taskInput = document.querySelector('.taskInput');
+export const addTaskButton = document.querySelector('.addTaskButton');
+export const taskList = document.querySelector('.taskList');
+export const showAllTasksButton = document.querySelector('.all__tasks')
+export const showCompletedTasksButton = document.querySelector('.completed__tasks');
+export const showIncompletedTasksButton = document.querySelector('.incompleted__tasks');
