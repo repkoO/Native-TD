9072de06cc51d06665ac5ddad3ee8de36ee3c99d
Kirill Medvedev <repkomail@gmail.com>
2024-02-15 22:41:45 +0200

    fix: fix styles and js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,7 +5,7 @@ import {
     showAllTasksButton,
     showCompletedTasksButton,
     showIncompletedTasksButton }
-  from './declaration.js';
+  from '/assets/scripts/declaration.js';
 
   document.addEventListener('DOMContentLoaded', () => {
     // Загрузка данных из localStorage
@@ -14,6 +14,7 @@ import {
       addTaskToList(task);
     });
 
+    console.log(addTaskButton);
 
     addTaskButton.addEventListener('click', () => {
       const taskText = taskInput.value.trim();
